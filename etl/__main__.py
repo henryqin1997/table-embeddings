2e7d1beb61b819ea7a7955433f32848017033ba9
Zian Ke <kezian@umich.edu>
2018-09-15 14:29:45 -0400

    Store all tables to list

--- etl/__main__.py
+++ etl/__main__.py
@@ -16,23 +16,25 @@ def satisfy_variants(data):
 
 
 def main():
-    # print(st.tag('Michael Cafarella is doing a project on Tables Embedding in September 2018'.split()))
+    tables = []
     with open(os.path.join(data_dir, 'sample'), encoding='utf-8') as f:
         for line in f:
             data = json.loads(line)
             if satisfy_variants(data) and len(data['relation']) in range(10, 20) and len(data['relation'][0]) in range(
                     10, 20):
-                table = Table(data)
-                if table.get_data_md5() == '0abc1124b6766c9bf281982a4e6adc5e':
-                    json.dump(data,
-                              open(os.path.join(data_dir, 'table-{}.json'.format(table.get_data_md5())), 'w+'),
-                              indent=4)
-                    print(table.get_header())
-                    print(table.get_entities())
-                    print(table.get_attributes())
-                    print(table.generate_ner_matrix(st, tag_to_index))
-                    print(table.generate_wordlist_matrix({'Date': 0, 'Name': 1, 'Opponent': 2}))
-                    print()
+                tables.append(Table(data))
+
+    for table in tables:
+        if table.get_data_md5() == '0abc1124b6766c9bf281982a4e6adc5e':
+            json.dump(data,
+                      open(os.path.join(data_dir, 'table-{}.json'.format(table.get_data_md5())), 'w+'),
+                      indent=4)
+            print(table.get_header())
+            print(table.get_entities())
+            print(table.get_attributes())
+            print(table.generate_ner_matrix(st, tag_to_index))
+            print(table.generate_wordlist_matrix({'Date': 0, 'Name': 1, 'Opponent': 2}))
+            print()
 
 
 if __name__ == '__main__':
