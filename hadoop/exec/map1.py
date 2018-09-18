8a1d43784b60b71c5564b20392e62a4adfe3da23
Zian Ke <kezian@umich.edu>
2018-09-18 12:15:20 -0400

    Copy functions from table

--- hadoop/exec/map1.py
+++ hadoop/exec/map1.py
@@ -3,14 +3,25 @@
 
 import sys
 import json
-from etl import Table
-from etl import satisfy_variants
+import numpy
+
+
+def satisfy_variants(data):
+    return data['tableType'] == 'RELATION' and data['hasHeader'] and data['headerRowIndex'] >= 0 and data[
+        'tableOrientation'] in ['HORIZONTAL', 'VERTICAL']
+
+
+def extract_header(data):
+    if data['tableOrientation'] == 'HORIZONTAL':
+        return numpy.array([item[data['headerRowIndex']] for item in data['relation']])
+    else:
+        return numpy.array(data['relation'][data['headerRowIndex']])
+
 
 for line in sys.stdin:
     data = json.loads(line)
     if satisfy_variants(data):
-        table = Table(data)
-        words = table.get_header()
+        words = extract_header(data)
         for word in words:
             word = word.lower().strip()
             if len(word) > 2:
