09f0f3bab56a20a2f2e6a8c926fb9404c4610dea
henryqin1997 <37519855+henryqin1997@users.noreply.github.com>
2018-09-19 14:41:06 -0400

    Add files via upload
    put variables in class for future use

--- nn/neural.py
+++ nn/neural.py
@@ -3,22 +3,22 @@
 
 """The 0.1 version of the training network for Tables-Embedding Project"""
 
-import torch.save
 import numpy as np
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from .__main__ import batch_size
 
-COLUMN_DATA_TYPES = 7  # NUMBER OF POSSIBLE TYPES OF DATA IN A COLUMN OF A TABLE
-WORDLIST_LABEL_SIZE = 300  # NUMBER OF DIFFERENT LABELS FOR WHOLE TRAINING SET
+from .__main__ import batch_size
 
 
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, COLUMN_DATA_TYPES=7, WORDLIST_LABEL_SIZE=4356):
         super(Net, self).__init__()
         # COLUMN_DATA_TYPES*10 input, 2 internal layer, 1*10 output
-        self.fc1 = nn.Linear(COLUMN_DATA_TYPES * 10, 2 * WORDLIST_LABEL_SIZE * 10)
-        self.fc2 = nn.Linear(2 * WORDLIST_LABEL_SIZE * 10, WORDLIST_LABEL_SIZE * 10)
+        self.COLUMN_DATA_TYPES = COLUMN_DATA_TYPES
+        self.WORDLIST_LABEL_SIZE = WORDLIST_LABEL_SIZE
+        self.fc1 = nn.Linear(self.COLUMN_DATA_TYPES * 10, 2 * self.WORDLIST_LABEL_SIZE * 10)
+        self.fc2 = nn.Linear(2 * self.WORDLIST_LABEL_SIZE * 10, self.WORDLIST_LABEL_SIZE * 10)
 
     def forward(self, x):
         x = self.fc1(x)
@@ -27,27 +27,36 @@ class Net(nn.Module):
         return x
 
     def normalize(self, x):
-        x.view(-1, WORDLIST_LABEL_SIZE)
+        x.view(-1, self.WORDLIST_LABEL_SIZE)
         xn = F.normalize(x, p=2, dim=1)
         xn = xn.view(-1)
         return xn
 
-def save_model(model):
-    model.save_state_dict("mytraining.pt")
+    def word_size(self):
+        return self.WORDLIST_LABEL_SIZE
 
-def load_model(model):
-    model.load_state_dict(torch.load("mytraining.pt"))
+    def column_types(self):
+        return self.COLUMN_DATA_TYPES
 
-def predict(net, input, ifbatch = False):
+
+def save_model(model, name):
+    model.save_state_dict(name)
+
+
+def load_model(model, name):
+    model.load_state_dict(torch.load(name))
+
+
+def predict(net, input, ifbatch=False):
     if ifbatch:
         output_ = []
         for i in range(batch_size):
             values, indices = torch.max(net(input), 0)
-            output = np.zeros([10, WORDLIST_LABEL_SIZE])
+            output = np.zeros([10, net.word_size()])
             i = 0
             for indice in indices:
                 output[0][indice] = 1
-                i = i +1
+                i = i + 1
             output_.append(output)
         return output_
 
