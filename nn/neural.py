1493c59fc5bba51cd3f1ac2abb48630d9316501b
henryqin1997 <37519855+henryqin1997@users.noreply.github.com>
2018-09-19 13:12:40 -0400

    Add files via upload

--- nn/neural.py
+++ nn/neural.py
@@ -4,8 +4,10 @@
 """The 0.1 version of the training network for Tables-Embedding Project"""
 
 import torch.save
+import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
+from .__main__ import batch_size
 
 COLUMN_DATA_TYPES = 7  # NUMBER OF POSSIBLE TYPES OF DATA IN A COLUMN OF A TABLE
 WORDLIST_LABEL_SIZE = 300  # NUMBER OF DIFFERENT LABELS FOR WHOLE TRAINING SET
@@ -35,3 +37,20 @@ def save_model(model):
 
 def load_model(model):
     model.load_state_dict(torch.load("mytraining.pt"))
+
+def predict(net, input, ifbatch = False):
+    if ifbatch:
+        output_ = []
+        for i in range(batch_size):
+            values, indices = torch.max(net(input), 0)
+            output = np.zeros([10, WORDLIST_LABEL_SIZE])
+            i = 0
+            for indice in indices:
+                output[0][indice] = 1
+                i = i +1
+            output_.append(output)
+        return output_
+
+    else:
+        output = net(input)
+        return output
