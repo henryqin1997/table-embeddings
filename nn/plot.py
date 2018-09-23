eb617e37dae0f0fefe702d34fb9318178d813e5b
Bingye Li <libingye@umich.edu>
2018-09-22 22:37:35 -0400

    plot&accuracy func

--- nn/plot.py
+++ nn/plot.py
@@ -1,5 +1,11 @@
 import matplotlib.pyplot as plt
 
-def plot_tvsv():
+def plot_tvsv(training_accuracy,validation_accuracy,batch_size):
     #plot the training vs. validation accuracy, to be implemented
+    batch=[(i+1)*batch_size for i in range(len(training_accuracy))]
+    plt.plot(batch,training_accuracy,'b-',label="training accuracy")
+    plt.plot(batch,validation_accuracy,'r-',label="validation accuracy")
+    plt.legend("training&validation accuracy plot")
+    plt.xlabel("batch number")
+    plt.show()
     return 0
