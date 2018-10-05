51efd0bd18d9530fb27639248a7d259f103a0d7f
Zian Ke <kezian@umich.edu>
2018-10-05 18:14:57 -0400

    Fix bug to handle each matrix in a batch

--- nn/train.py
+++ nn/train.py
@@ -24,29 +24,35 @@ def load_data(batch_size, batch_index=0):
     batch_files = training_files[batch_size * batch_index:batch_size * (batch_index + 1)]
     batch_files_ner = list(map(lambda batch_file: batch_file.rstrip('.json') + '_ner.csv', batch_files))
     batch_files_wordlist = list(map(lambda batch_file: batch_file.rstrip('.json') + '_wordlist.csv', batch_files))
-    input = numpy.array(
+    inputs = numpy.array(
         [numpy.genfromtxt(os.path.join(training_data_dir, batch_file_ner), delimiter=',') for batch_file_ner in
          batch_files_ner])
-    target = numpy.array(
+    targets = numpy.array(
         [numpy.genfromtxt(os.path.join(training_data_dir, batch_file_wordlist), delimiter=',') for batch_file_wordlist
          in batch_files_wordlist])
-    assert len(input) == len(tag_to_index)
 
     # Use One Hot Encoding and remove column with all zeros
-    input = numpy.array([one_hot(row) for row in input.transpose()])
-    target = target.transpose()
+    for i in range(len(inputs)):
+        input = inputs[i]
+        target = targets[1]
+        assert len(input) == len(tag_to_index)
 
-    input_transformed = numpy.zeros(input.shape)
-    target_transformed = numpy.zeros(target.shape)
+        input = numpy.array([one_hot(row) for row in input.transpose()])
+        target = target.transpose()
 
-    current_index = 0
-    for row in input:
-        if row[0] == 0:
-            input_transformed[current_index] = input[current_index]
-            target_transformed[current_index] = target[current_index]
-            current_index += 1
+        input_transformed = numpy.zeros(input.shape)
+        target_transformed = numpy.zeros(target.shape)
 
-    return input_transformed.transpose(), target_transformed.transpose()
+        current_index = 0
+        for row in input:
+            if row[0] == 0:
+                input_transformed[current_index] = input[current_index]
+                target_transformed[current_index] = target[current_index]
+                current_index += 1
+
+        inputs[i] = input_transformed.transpose()
+        targets[i] = target_transformed.transpose()
+    return inputs, targets
 
 
 ##########################333#3#
