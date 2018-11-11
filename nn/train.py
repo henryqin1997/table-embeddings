1064c150b2a1c3ec3c25192b997f67a332cf209e
Bingye Li <libingye@umich.edu>
2018-11-10 22:12:57 -0500

    add sample_dict()

--- nn/train.py
+++ nn/train.py
@@ -58,7 +58,35 @@ def load_data(batch_size, batch_index=0):
         targets_transformed.append(target_transformed.transpose())
     return numpy.array(inputs_transformed), numpy.array(targets_transformed)
 
-
+def sample_dict(sample_feature,sample_target,sample_active,iteration):
+    batch_size=len(sample_feature)/iteration
+    missed_feature=[]
+    with open('diction_prediction.json', 'r') as fp:
+        prediction=json.load(fp)
+    for batch in range(iteration):
+        sample_summary=defaultdict(lambda:[0,0])
+        features=sample_feature[batch*batch_size:batch_size*(batch+1)]
+        targets=sample_target[batch*batch_size:batch_size*(batch+1)]
+        actives=sample_active[batch*batch_size:batch_size*(batch+1)]
+        for index in range(batch_size):
+            feature_split=features[index].split(',')
+            target_split=targets[index].split(',')
+            if features[index] not in prediction:
+                for i in range(10):
+                    if actives[index][i]==1 and feature_split[i]!='-1':
+                        sample_summary[target_split[i]][1]+=1
+                missed_feature.append(features[index])
+            else:
+                for i in range(10):
+                    if actives[index][i]==1 and feature_split[i]!='-1':
+                        sample_summary[target_split[i]][1]+=1
+                        if target_split[i]==prediction[features[index]][i]:
+                            sample_summary[target_split[i]][0]+=1
+        with open("sample_dict_batch={}".format(batch),'w') as wfp:
+            json.dump(sample_summary,wfp)
+    with open("miss_features_it={}".format(batch),'w') as wfp:
+        for f in missed_feature:
+            wfp.write(f+"\n")
 ##########################333#3#
 # evaluation of model:
 # 1. accuracy of prediction of label over target     #correct prediction/#targetlabel
