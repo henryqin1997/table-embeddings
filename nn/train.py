dea07ad91e4c8cf4255cbeaec861d457e3bcd8e5
henryqin <henryqin@umich.edu>
2018-11-12 12:21:53 -0500

    debug info

--- nn/train.py
+++ nn/train.py
@@ -98,8 +98,7 @@ def load_sample_random_lable(sample_index, batch_size, batch_index):
         result.append([input_transformed, target_transformed, activate_transformed])
     return result
 
-
-def sample_dict(sample_data,sample_summary,missed_feature):
+def sample_dict(sample_data,sample_summary,missed_feature,faultdic):
     batch_size=len(sample_data)
     #missed_feature=[]
     with open('nn/diction_prediction.json', 'r') as fp:
@@ -110,6 +109,7 @@ def sample_dict(sample_data,sample_summary,missed_feature):
     #     features=sample_feature[batch*batch_size:batch_size*(batch+1)]
     #     targets=sample_target[batch*batch_size:batch_size*(batch+1)]
     #     actives=sample_active[batch*batch_size:batch_size*(batch+1)]
+
     for index in range(batch_size):
         feature=sample_data[index][0]
         target=sample_data[index][1]
@@ -125,6 +125,13 @@ def sample_dict(sample_data,sample_summary,missed_feature):
                     sample_summary[target[i]][1]+=1
                     if target[i]==int(prediction[','.join(str(x) for x in feature)].split(',')[i]):
                         sample_summary[target[i]][0]+=1
+                    else:
+                        mispred = [str(x) for x in target]
+                        for i in range(10):
+                            if activate[i]==1:
+                                mispred[i]+='*'
+                        mispred=','.join(mispred)
+                        faultdic[','.join(str(x) for x in feature)].append(mispred)
     # with open("sample_dict_batch={}".format(batch_index),'w') as wfp:
     #         json.dump(sample_summary,wfp)
     #
@@ -132,22 +139,45 @@ def sample_dict(sample_data,sample_summary,missed_feature):
     #     for f in missed_feature:
     #         wfp.write(f+"\n")
 
+
 def sample_print():
     batch_size=50
     sample_size=4500
     missed_feature=set([])
+    faultdic = defaultdict(lambda: [])
+
     for sample_index in range(10):
         sample_summary = defaultdict(lambda: [0, 0])
         batch_index = 0
         while batch_size*batch_index<sample_size:
             sample_data=load_sample_random_lable(sample_index,batch_size,batch_index)
-            sample_dict(sample_data,sample_summary,missed_feature)
+            sample_dict(sample_data,sample_summary,missed_feature,faultdic)
             batch_index+=1
         with open("nn/sample_dict_it={}".format(sample_index), 'w') as wfp:
             json.dump(sample_summary, wfp)
     with open("nn/miss_features",'w') as wfp:
         for f in missed_feature:
             wfp.write(f+"\n")
+    with open("nn/fault_diction.json",'w') as wfp:
+        json.dump(faultdic,wfp)
+
+
+def sample_print_uniform():
+    batch_size = 50
+    sample_size = 4500
+    missed_feature = set([])
+    sample_summary = defaultdict(lambda: [0, 0])
+    batch_index = 0
+    while batch_size * batch_index < sample_size:
+        sample_data = load_sample_random_lable(sample_index, batch_size, batch_index)
+        sample_dict(sample_data, sample_summary, missed_feature)
+        batch_index += 1
+    with open("nn/sample_dict_it={}".format(sample_index), 'w') as wfp:
+        json.dump(sample_summary, wfp)
+    with open("nn/miss_features", 'w') as wfp:
+        for f in missed_feature:
+            wfp.write(f + "\n")
+
 ##########################333#3#
 # evaluation of model:
 # 1. accuracy of prediction of label over target     #correct prediction/#targetlabel
@@ -347,6 +377,9 @@ def measure_distribution_no_cut(diction, input, target):
 
 def main():
     sample_print()
+    diction_prediction=json.load()
+
+    print(len(list()))
     # dic = defaultdict(lambda: defaultdict(int))
     # dic_no_cut = defaultdict(lambda: defaultdict(int))
     # dic_prediction = defaultdict(lambda: '')
