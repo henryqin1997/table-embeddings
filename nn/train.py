639125e921b8b45a98b69d11abd631c7424f65ba
Bingye Li <libingye@umich.edu>
2018-11-12 00:21:51 -0500

    add sample_dict() and sample_print()

--- nn/train.py
+++ nn/train.py
@@ -9,7 +9,8 @@ training_files_json = 'data/training_files_filtered.json'
 training_files = json.load(open(training_files_json))
 testing_data_dir = 'data/sample_random_label_train'
 activate_data_dir = 'data/sample_random_label'
-testing_files_json = 'data/testing_files_random_label.json'
+#testing_files_json = 'data/testing_files_random_label.json'
+testing_files_json = 'data/testing_files_random_label_sample.json'
 testing_files = [[y for y in json.load(open(testing_files_json)) if y[0] == str(x)] for x in range(10)]
 tag_to_index = {'LOCATION': 0, 'PERSON': 1, 'ORGANIZATION': 2, 'MONEY': 3, 'PERCENT': 4, 'DATE': 5, 'TIME': 6}
 
@@ -115,12 +116,12 @@ def sample_dict(sample_data,sample_summary,missed_feature):
         activate=sample_data[index][2]
         if ','.join(feature) not in prediction:
             for i in range(10):
-                if activate[i]==1 and feature[i]!=-1:
+                if activate[i]==1 and target[i]!=-1:
                     sample_summary[target[i]][1]+=1
             missed_feature.add(','.join(feature))
         else:
             for i in range(10):
-                if activate[i]==1 and feature[i]!=-1:
+                if activate[i]==1 and target[i]!=-1:
                     sample_summary[target[i]][1]+=1
                     if target[i]==prediction[','.join(feature)][i]:
                         sample_summary[target[i]][0]+=1
