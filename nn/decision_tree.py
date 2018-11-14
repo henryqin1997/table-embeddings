b5c6009516ea42e79d70b10ec7b310806345bb5d
henryqin <henryqin@umich.edu>
2018-11-13 20:35:40 -0500

    debug info

--- nn/decision_tree.py
+++ nn/decision_tree.py
@@ -9,9 +9,8 @@ training_files_json = 'data/training_files_filtered.json'
 training_files = json.load(open(training_files_json))
 testing_data_dir = 'data/sample_random_label_train'
 activate_data_dir = 'data/sample_random_label'
-testing_files_json = 'data/testing_files_random_label.json'
-testing_files_json = 'data/testing_files_random_label_sample.json'
-testing_files = [[y for y in json.load(open(testing_files_json)) if y[0] == str(x)] for x in range(10)]
+#testing_files_json = 'data/testing_files_random_label.json'
+#testing_files = [[y for y in json.load(open(testing_files_json)) if y[0] == str(x)] for x in range(10)]
 tag_to_index = {'LOCATION': 0, 'PERSON': 1, 'ORGANIZATION': 2, 'MONEY': 3, 'PERCENT': 4, 'DATE': 5, 'TIME': 6}
 
 def measure_distribution_no_cut(diction, input, target):
