f972b9a2d0a316a7543ffd3e04c880c9f1798924
Zian Ke <kezian@umich.edu>
2018-11-13 20:39:30 -0500

    Copy load_data into decision tree

--- nn/train.py
+++ nn/train.py
@@ -53,25 +53,22 @@ def load_data(batch_size, batch_index=0):
         target = targets[i]
         assert len(input) == len(tag_to_index)
 
-        # input = numpy.array([one_hot(row) for row in input.transpose()])
-        # target = target.transpose()
-        #
-        # # Remove all zero columns
-        # input_transformed = numpy.zeros(input.shape)
-        # target_transformed = numpy.zeros(target.shape)
-        #
-        # current_index = 0
-        # for row in input:
-        #     if row[0] == 0:
-        #         input_transformed[current_index] = input[current_index]
-        #         target_transformed[current_index] = target[current_index]
-        #         current_index += 1
-        #
-        # inputs_transformed.append(input_transformed.transpose())
-        # targets_transformed.append(target_transformed.transpose())
+        input = numpy.array([one_hot(row) for row in input.transpose()])
+        target = target.transpose()
 
-        inputs_transformed.append(numpy.array([one_hot(row) for row in input.transpose()]).transpose())
-        targets_transformed.append(target)
+        # Remove all zero columns
+        input_transformed = numpy.zeros(input.shape)
+        target_transformed = numpy.zeros(target.shape)
+
+        current_index = 0
+        for row in input:
+            if row[0] == 0:
+                input_transformed[current_index] = input[current_index]
+                target_transformed[current_index] = target[current_index]
+                current_index += 1
+
+        inputs_transformed.append(input_transformed.transpose())
+        targets_transformed.append(target_transformed.transpose())
     return numpy.array(inputs_transformed), numpy.array(targets_transformed)
 
 
