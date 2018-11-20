a2e035272bbc1b48b4bc06b5297bd86aff15d3b5
Zian Ke <kezian@umich.edu>
2018-11-19 19:17:09 -0500

    Add row constraint

--- etl/100k.py
+++ etl/100k.py
@@ -1,10 +1,12 @@
 import json
 import os
 import random
+from .table import Table, satisfy_variants
 
 random.seed(10)
 
 num_folders = 51
+min_num_rows = 5
 all_files = []
 
 if __name__ == '__main__':
@@ -12,6 +14,10 @@ if __name__ == '__main__':
         folder = str(folder)
         print(folder)
         for file in os.listdir(os.path.join('webtables', folder)):
-            if random.random() < 0.0025:
-                all_files.append(os.path.join(folder, file))
+            if random.random() < 0.005:
+                data = json.load(open(os.path.join('webtables', folder, file)))
+                if satisfy_variants(data):
+                    table = Table(data)
+                    if len(table.get_entities()) >= min_num_rows:
+                        all_files.append(os.path.join(folder, file))
     json.dump(all_files, open('temp_filelist.json', 'w+'), indent=4)
