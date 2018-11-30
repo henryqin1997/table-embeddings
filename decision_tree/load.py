02969ee4e825b67dceca837448acc9c6c80c0274
Zian Ke <kezian@umich.edu>
2018-11-30 15:10:01 -0500

    Two load_data

--- decision_tree/load.py
+++ decision_tree/load.py
@@ -32,6 +32,36 @@ def load_data(batch_size, batch_index=0):
     # load training data from file, to be implemented
     # put size number of data into one array
     # start from batch_index batch
+    # return two arrays: input, target
+    batch_files = training_files[batch_size * batch_index:batch_size * (batch_index + 1)]
+    batch_files_ner = list(map(lambda batch_file: batch_file.rstrip('.json') + '_ner.csv', batch_files))
+    batch_files_wordlist = list(map(lambda batch_file: batch_file.rstrip('.json') + '_wordlist.csv', batch_files))
+    inputs = numpy.array(
+        [numpy.genfromtxt(os.path.join(training_data_dir, batch_file_ner), delimiter=',') for batch_file_ner in
+         batch_files_ner])
+    targets = numpy.array(
+        [numpy.genfromtxt(os.path.join(training_data_dir, batch_file_wordlist), delimiter=',') for batch_file_wordlist
+         in batch_files_wordlist])
+
+    inputs_transformed = []
+    targets_transformed = []
+
+    # Use One Hot Encoding
+    for i in range(len(inputs)):
+        input = inputs[i]
+        target = targets[i]
+        assert len(input) == len(tag_to_index)
+
+        inputs_transformed.append(numpy.array([one_hot(row) for row in input.transpose()]).transpose())
+        targets_transformed.append(target)
+    return numpy.array(inputs_transformed), numpy.array(targets_transformed)
+
+
+def load_data_with_raw(batch_size, batch_index=0):
+    # load training data from file, to be implemented
+    # put size number of data into one array
+    # start from batch_index batch
+    # return two arrays: raw, input, target
     batch_files = training_files[batch_size * batch_index:batch_size * (batch_index + 1)]
     batch_files_ner = list(map(lambda batch_file: batch_file.rstrip('.json') + '_ner.csv', batch_files))
     batch_files_wordlist = list(map(lambda batch_file: batch_file.rstrip('.json') + '_wordlist.csv', batch_files))
