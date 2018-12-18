e30a178dc3cd91974eab478eff959737c9adb2c3
Zian Ke <kezian@umich.edu>
2018-12-18 13:12:34 -0500

    Output file list

--- etl/domain_schema.py
+++ etl/domain_schema.py
@@ -7,7 +7,8 @@ from urllib.parse import urlparse
 from etl.table import Table
 
 files = json.load(open('data/training_files.json'))
-url_dict = defaultdict(int)
+domain_schema_dict = defaultdict(int)
+domain_schema_files_dict = {}
 
 for file in files:
     data = json.load(open(os.path.join('data/train', file)))
@@ -19,8 +20,13 @@ for file in files:
         result = '{uri.scheme}://{uri.netloc}/ {schema}'.format(uri=parsed_uri,
                                                                 schema=[label.lower() for label in table.get_header()])
 
-        url_dict[result] += 1
+        domain_schema_dict[result] += 1
+        if domain_schema_dict[result] == 1:
+            domain_schema_files_dict[result] = file
 
-url_dict = sorted(url_dict.items(), key=itemgetter(1), reverse=True)
+domain_schema_dict = sorted(domain_schema_dict.items(), key=itemgetter(1), reverse=True)
+domain_schema_files_dict = sorted(domain_schema_files_dict.items(), key=lambda item: domain_schema_dict[item[0]],
+                                  reverse=True)
 
-json.dump(url_dict, open('data/domain_schema_dict.json', 'w+'), indent=4)
+json.dump(domain_schema_dict, open('data/domain_schema_dict.json', 'w+'), indent=4)
+json.dump(domain_schema_files_dict, open('data/domain_schema_files_dict.json', 'w+'), indent=4)
