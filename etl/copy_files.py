f2ad1e53600c7f86d4d4c016f9aca97762416d68
Zian Ke <kezian@umich.edu>
2018-12-18 13:24:33 -0500

    To copy files

---
+++ etl/copy_files.py
@@ -0,0 +1,19 @@
+import json
+import sys
+import shutil
+import os
+
+files_json = sys.argv[1]
+input_dir = sys.argv[2]
+output_dir = sys.argv[3]
+
+if __name__ == '__main__':
+    files = json.load(files_json)
+    if type(files) is dict:
+        files = files.values()
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    for file in files:
+        shutil.copyfile(os.path.join(input_dir, file), os.path.join(output_dir, file))
