da2c9b2c8a35b019d5f68232454cf479555750e7
Zian Ke <kezian@umich.edu>
2018-12-22 20:39:11 -0500

    Identify nst

--- etl/feature_identifier.py
+++ etl/feature_identifier.py
@@ -1,6 +1,7 @@
 from dateutil.parser import parse
 import json
 import os
+import re
 
 webtables_dir = './webtables'
 training_data_dir = './data/train'
@@ -12,6 +13,22 @@ def identify_date(value):
         return True
     except ValueError:
         return False
+
+
+def identify_number(value):
+    return bool(re.search(r'\d+', value))
+
+
+def identify_symbol(value):
+    return bool(re.search(r'[^a-zA-Z0-9]+', value))
+
+
+def identify_text(value):
+    return bool(re.search(r'[a-zA-Z]+', value))
+
+
+def identify_nst(value):
+    return [identify_number(value), identify_symbol(value), identify_text(value)]
 
 
 # def identify_features(training_files):
@@ -37,4 +54,4 @@ def identify_date(value):
 
 if __name__ == '__main__':
     value = '08/22/2015 - 10:00am'
-    print(identify_date(value))
+    print(identify_nst('12423.5'))
