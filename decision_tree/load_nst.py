7ee2da2383ea503001d98ae93cc2bef1fe35539b
Zian Ke <kezian@umich.edu>
2018-12-24 03:29:19 -0500

    Finish load_nst_max

--- decision_tree/load_nst.py
+++ decision_tree/load_nst.py
@@ -9,7 +9,6 @@ training_files = json.load(open(training_files_json))
 
 
 def load_nst_major(batch_size, batch_index):
-    '''To be implemented'''
     batch_files = training_files[batch_size * batch_index:batch_size * (batch_index + 1)]
     batch_files_nst = list(map(lambda batch_file: batch_file.rstrip('.json') + '_nst.csv', batch_files))
     batch_files_wordlist = list(map(lambda batch_file: batch_file.rstrip('.json') + '_wordlist.csv', batch_files))
@@ -34,11 +33,30 @@ def load_nst_major(batch_size, batch_index):
 
 
 def load_nst_max(batch_size, batch_index):
-    '''To be implemented'''
+    batch_files = training_files[batch_size * batch_index:batch_size * (batch_index + 1)]
+    batch_files_nst = list(map(lambda batch_file: batch_file.rstrip('.json') + '_nst.csv', batch_files))
+    batch_files_wordlist = list(map(lambda batch_file: batch_file.rstrip('.json') + '_wordlist.csv', batch_files))
+    inputs = numpy.array(
+        [numpy.genfromtxt(os.path.join(training_data_dir, batch_file_nst), delimiter=',')[1] for batch_file_nst in
+         batch_files_nst])
+    targets = numpy.array(
+        [numpy.genfromtxt(os.path.join(training_data_dir, batch_file_wordlist), delimiter=',') for batch_file_wordlist
+         in batch_files_wordlist])
+
+    targets_transformed = []
+
+    for i in range(len(targets)):
+        table = Table(json.load(open(os.path.join(training_data_dir, batch_files[i]))))
+        column_num = len(table.get_header())
+        target = targets[i]
+
+        targets_transformed.append(
+            numpy.array([index_of(list(map(lambda num: int(round(num)), row)), 1) if idx < column_num else -1 for
+                         idx, row in enumerate(target.transpose())]).transpose())
+    return inputs, numpy.array(targets_transformed)
 
 
 def load_nst_overall(batch_size, batch_index):
-    '''To be implemented'''
 
 
 def load_nst_majo(batch_size, batch_index):
