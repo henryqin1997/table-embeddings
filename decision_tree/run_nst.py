6d94736cfa6b0dde421454c49df2b0f31e62089a
Zian Ke <kezian@umich.edu>
2018-12-24 13:55:36 -0500

    Rerun train

--- decision_tree/run_nst.py
+++ decision_tree/run_nst.py
@@ -7,54 +7,58 @@ from .decision_tree import diction_pred
 from .decision_tree import label_num_arr, label_num_str, correct_pred
 from .load_nst import *
 
+
 def measure_distribution_cut(diction, input, target):
     for i in range(len(input)):
-        if input[i]!=-1:
-            diction[str(input[i])][str(target[i])]+=1
+        if input[i] != -1:
+            diction[str(input[i])][str(target[i])] += 1
         else:
             break
+
 
 def measure_distribution_no_cut(diction, input, target):
     input = [str(x) for x in input]
     target = [str(x) for x in target]
     diction[','.join(input)][','.join(target)] += 1
 
+
 def generate_dic_pred():
-    prediction=defaultdict(lambda: [])
-    with open('decision_tree/diction.json','r') as fp:
+    prediction = defaultdict(lambda: [])
+    with open('decision_tree/diction.json', 'r') as fp:
         dic = json.load(fp)
     for key in dic.keys():
-        length=len([x for x in key.split(',') if x!='-1'])
-        findmax=[defaultdict(int) for i in range(length)]
+        length = len([x for x in key.split(',') if x != '-1'])
+        findmax = [defaultdict(int) for i in range(length)]
         for seckey in dic[key].keys():
-            seckey_transformed=[x for x in seckey.split(',')]
+            seckey_transformed = [x for x in seckey.split(',')]
             for i in range(length):
-                findmax[i][seckey_transformed[i]]+=dic[key][seckey]
+                findmax[i][seckey_transformed[i]] += dic[key][seckey]
         maxlabel = ['-1' for i in range(10)]
         maxcount = [0 for i in range(10)]
         for i in range(10):
-            if i<length:
-                total=0
-                for label,count in findmax[i].items():
-                    total+=count
-                    if count>maxcount[i]:
-                        maxcount[i]=count
-                        maxlabel[i]=label
-                maxcount[i]=float(maxcount[i]/total)
+            if i < length:
+                total = 0
+                for label, count in findmax[i].items():
+                    total += count
+                    if count > maxcount[i]:
+                        maxcount[i] = count
+                        maxlabel[i] = label
+                maxcount[i] = float(maxcount[i] / total)
             else:
-                maxcount[i]=1.0
+                maxcount[i] = 1.0
         prediction[key].append(','.join(maxlabel))
         prediction[key].append(maxcount)
-    with open('decision_tree/diction_prediction_advanced.json','w') as fp:
-        json.dump(prediction,fp)
+    with open('decision_tree/diction_prediction_advanced.json', 'w') as fp:
+        json.dump(prediction, fp, indent=4)
+
 
 def train():
     dic_cut = defaultdict(lambda: defaultdict(int))
     dic_no_cut = defaultdict(lambda: defaultdict(int))
     dic_prediction = defaultdict(lambda: '')
 
-    for func_index,load_data in enumerate(
-            [load_nst_major,load_nst_max,load_nst_overall,load_nst_mm,load_nst_majo,load_nst_maxo,load_nst_mmo]):
+    for func_index, load_data in enumerate(
+            [load_nst_major, load_nst_max, load_nst_overall, load_nst_mm, load_nst_majo, load_nst_maxo, load_nst_mmo]):
 
         train_size = 100000
         batch_size = 50
@@ -68,11 +72,11 @@ def train():
                 measure_distribution_no_cut(dic_no_cut, input[i], target[i])
 
         with open('decision_tree/diction_nst{}.json'.format(func_index), 'w') as fp:
-            json.dump(dic_no_cut, fp)
+            json.dump(dic_no_cut, fp, indent=4)
             print('diction_nst{} saved'.format(func_index))
 
         with open('decision_tree/diction_nst_cut{}.json'.format(func_index), 'w') as fp:
-            json.dump(dic_cut, fp)
+            json.dump(dic_cut, fp, indent=4)
             print('diction_cut{} saved'.format(func_index))
 
         print('table')
@@ -88,7 +92,7 @@ def train():
             maxlabel = ''
             for label in dic_no_cut[key].keys():
                 if label != '-1,-1,-1,-1,-1,-1,-1,-1,-1,-1':
-                    sum += dic_no_cut[key][label]*label_num_str(label)
+                    sum += dic_no_cut[key][label] * label_num_str(label)
                     if dic_no_cut[key][label] > max:
                         max = dic_no_cut[key][label]
                         maxlabel = label
@@ -98,14 +102,14 @@ def train():
 
             dic_prediction[key] = maxlabel
             for label in dic_no_cut[key].keys():
-                pre_acc += dic_no_cut[key][label] * correct_pred(maxlabel,label)
+                pre_acc += dic_no_cut[key][label] * correct_pred(maxlabel, label)
 
         print("train accuracy {}".format(pre_acc / sum))
 
         with open('decision_tree/diction_nst_prediction{}.json'.format(func_index), 'w') as fp:
-            json.dump(dic_prediction, fp)
+            json.dump(dic_prediction, fp, indent=4)
             print('diciton prediction{} saved'.format(func_index))
-        #generate_dic_pred()
+        # generate_dic_pred()
 
         dic_cut_pred = defaultdict(lambda: ['', 0.])
         for key1 in dic_cut.keys():
@@ -120,7 +124,7 @@ def train():
             dic_cut_pred[key1] = [maxlabel, float(max / sum_num)]
 
         with open('decision_tree/dic_nst_cut_pred{}.json'.format(func_index), 'w') as fp:
-            json.dump(dic_cut_pred, fp)
+            json.dump(dic_cut_pred, fp, indent=4)
 
         print('validating')
         batch_size = 50
@@ -146,10 +150,5 @@ def train():
         print('validation {} accuracy {}'.format(func_index, correct / total))
 
 
-
-if __name__=='__main__':
-    #train()
-    for func_index, load_data in enumerate(
-            [load_nst_major, load_nst_max, load_nst_overall, load_nst_mm, load_nst_majo, load_nst_maxo, load_nst_mmo]):
-        print(func_index)
-        print(load_data(50,0))
+if __name__ == '__main__':
+    train()
