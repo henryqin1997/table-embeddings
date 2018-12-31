4e1e9d024b1e54e050aa9f3a9a6bbea21e1b2d46
Zian Ke <kezian@umich.edu>
2018-12-31 18:48:45 -0500

    Load major+max+overall

--- decision_tree/load_nst.py
+++ decision_tree/load_nst.py
@@ -93,7 +93,34 @@ def load_nst_mm(batch_size, batch_index):
 
 
 def load_nst_mmo(batch_size, batch_index):
-    '''To be implemented'''
+    batch_files = training_files[batch_size * batch_index:batch_size * (batch_index + 1)]
+    batch_files_nst = list(map(lambda batch_file: batch_file.rstrip('.json') + '_nst.csv', batch_files))
+    batch_files_wordlist = list(map(lambda batch_file: batch_file.rstrip('.json') + '_wordlist.csv', batch_files))
+    inputs_major = [
+        list(map(to_int, numpy.genfromtxt(os.path.join(training_data_dir, batch_file_nst), delimiter=',')[0])) for
+        batch_file_nst in batch_files_nst]
+    inputs_max = [
+        list(map(to_int, numpy.genfromtxt(os.path.join(training_data_dir, batch_file_nst), delimiter=',')[1])) for
+        batch_file_nst in batch_files_nst]
+    inputs_overall = [
+        list(map(to_int, numpy.genfromtxt(os.path.join(training_data_dir, batch_file_nst), delimiter=',')[2])) for
+        batch_file_nst in batch_files_nst]
+    targets = numpy.array(
+        [numpy.genfromtxt(os.path.join(training_data_dir, batch_file_wordlist), delimiter=',') for batch_file_wordlist
+         in batch_files_wordlist])
+
+    targets_transformed = []
+
+    for i in range(len(targets)):
+        table = Table(json.load(open(os.path.join(training_data_dir, batch_files[i]))))
+        column_num = len(table.get_header())
+        target = targets[i]
+
+        targets_transformed.append(
+            numpy.array([index_of(list(map(lambda num: int(round(num)), row)), 1) if idx < column_num else -1 for
+                         idx, row in enumerate(target.transpose())]).transpose())
+    return numpy.array([[x[0] * 4 + x[1] * 2 + x[2] * 1 for x in zip(inputs[0], inputs[1], inputs[2])] for inputs in
+                        zip(inputs_major, inputs_max, inputs_overall)]), numpy.array(targets_transformed)
 
 
 def index_of(l, n):
@@ -108,4 +135,4 @@ def to_int(n):
 
 
 if __name__ == '__main__':
-    print(load_nst_major(50, 2060))
+    print(load_nst_mmo(50, 0))
