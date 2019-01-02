f591b126927b0ed71d125cdbdef157089bb121e7
Zian Ke <kezian@umich.edu>
2019-01-01 20:04:14 -0500

    Update function definition

--- numerical_space/load.py
+++ numerical_space/load.py
@@ -3,14 +3,14 @@ To load all numerical columns from tables.
 '''
 
 
-def summary():
+def summary(label, column):
     """
     Input: a column of numerical values in list version
-    Output: a list of label,mean,variance,min,max,ifordered,isreal
+    Output: a list of label, mean, variance, min, max, if_ordered, is_real
     """
 
 def load():
     """
-    return summaried data for a table, ie. a list of points summary.
+    return summarized data for a table, ie. a list of points summary.
     """
 
