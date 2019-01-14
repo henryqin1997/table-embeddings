cdd0b8a3fb9f5a139cb37efa28b7a8774da46c5e
henryqin <henryqin@umich.edu>
2019-01-13 20:29:49 -0500

    new space for numerical values

--- numerical_space/learn.py
+++ numerical_space/learn.py
@@ -1,6 +1,18 @@
 '''
 To build a space of numerical data points to predict label of them.
 '''
+from .load import load
+
+def build_space():
+    '''Build 6 diction, match (features)->(label,info)'''
+
+    train_size = 100000
+    batch_size = 50
+    batch_index = 0
+    while batch_size * batch_index < train_size:
+        print(batch_index)
+        summary = load(batch_size=batch_size, batch_index=batch_index)
+        batch_index += 1
 
 
 
