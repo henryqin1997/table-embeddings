01503b632110299fdbf2c7acf38e11ff5ed42e93
Zian Ke <kezian@umich.edu>
2019-01-16 16:21:44 -0500

    Linear prediction

--- numerical_space/falconn.py
+++ numerical_space/falconn.py
@@ -4,20 +4,33 @@ import timeit
 from numerical_space.load import load
 import itertools
 import json
+import math
 
 
 def falconn_test():
     train_size = 100
     batch_size = 50
     batch_index = 0
-    while batch_size * batch_index < train_size:
-        print(batch_index)
-        summary = load(batch_size=batch_size, batch_index=batch_index)
-        dataset = np.array([l[1:-2] for l in list(itertools.chain.from_iterable(summary))])
-        print(dataset)
-        dataset /= np.linalg.norm(dataset, axis=1).reshape(-1, 1)
-        print(dataset)
-        batch_index += 1
+    number_of_queries = 10
+    summary = [load(batch_size=batch_size, batch_index=batch_index) for batch_index in
+               range(int(math.floor(train_size / batch_size)))]
+    dataset = list(itertools.chain.from_iterable(itertools.chain.from_iterable(summary)))
+
+    # Filter out mean, variance, min, max all zero
+    dataset = list(filter(lambda l: np.linalg.norm(l[1:5]) > 0, dataset))
+
+    dataset_asc_int = list(filter(lambda l: not l[6] and l[5] == 1, dataset))
+
+    vectors = np.array([l[1:5] for l in dataset_asc_int])
+    labels = np.array([l[0] for l in dataset_asc_int])
+    print(vectors)
+    print(labels)
+
+    answers = []
+    for query in vectors[-number_of_queries:]:
+        print(np.dot(vectors[:-number_of_queries], query).argmax())
+        answers.append(labels[np.dot(vectors[:-number_of_queries], query).argmax()])
+    print(answers)
 
 
 if __name__ == '__main__':
