695b0f23148cc1d7c4f224860769dd3d5865ccc4
Bingye Li <libingye@umich.edu>
2019-01-17 18:28:10 -0500

    analysis.py in numerical_space

--- numerical_space/analysis.py
+++ numerical_space/analysis.py
@@ -17,17 +17,19 @@ def main():
         dic_ran_int = json.load(fp)
     with open('numerical_space/numerical_dic_-1_int.json','r') as fp:
         dic_des_int = json.load(fp)
-    dic=dic_asc_fl
+    dics=[dic_asc_fl,dic_ran_fl,dic_des_fl,dic_asc_int,dic_ran_int,dic_des_int]
+    titles=["dic_1_float","dic_0_float","dic_-1_float","dic_1_int","dic_0_int","dic_-1_int"]
+    i=0# index of dic
 
-    labels=[('16','b', 'o'), ('137','y', '^'),('221','r', 'x')]
+    labels=[('26','b', 'o'), ('1023','y', '^'),('2357','r', 'x')]
     fig=plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     for label,color,marker in labels:
         X = []
         Y = []
         Z = []
-        for keys in dic:
-            if label in dic[keys]:
+        for keys in dics[i]:
+            if label in dics[i][keys]:
                 keyArr=list(map(float,keys.split(",")))
                 X.append(keyArr[0])
                 Y.append(keyArr[1])
@@ -36,6 +38,7 @@ def main():
     ax.set_xlabel('mean')
     ax.set_ylabel('variance')
     ax.set_zlabel('range')
+    plt.title(titles[i])
     ax.legend()
     plt.show()
 if __name__=="__main__":
