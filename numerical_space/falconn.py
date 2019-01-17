928ad37982ad2518acab6987f5949f510c4ca10d
Zian Ke <kezian@umich.edu>
2019-01-17 18:59:50 -0500

    Print progress

--- numerical_space/falconn.py
+++ numerical_space/falconn.py
@@ -10,8 +10,11 @@ def falconn_test():
     number_of_queries = 3000
 
     print('Load 103k data')
-    summary = [load(batch_size=batch_size, batch_index=batch_index) for batch_index in
-               range(int(math.floor(train_size / batch_size)))]
+    summary = []
+    for batch_index in range(int(math.floor(train_size / batch_size))):
+        print(batch_index)
+        summary.append(load(batch_size=batch_size, batch_index=batch_index))
+
     dataset = list(itertools.chain.from_iterable(itertools.chain.from_iterable(summary)))
 
     print('Filter data')
@@ -26,11 +29,14 @@ def falconn_test():
 
     print('Predict last 3k')
     answers = []
-    for query in vectors[-number_of_queries:]:
+    for i, query in enumerate(vectors[-number_of_queries:]):
+        if i % 10 == 0:
+            print('{}/{}'.format(i, number_of_queries))
         answers.append(labels[np.dot(vectors[:-number_of_queries], query).argmax()])
 
     assert len(answers) == number_of_queries
-    print('Accuracy: {}/{}'.format(sum(i == j for i, j in zip(answers, labels[-number_of_queries:])), number_of_queries))
+    print(
+        'Accuracy: {}/{}'.format(sum(i == j for i, j in zip(answers, labels[-number_of_queries:])), number_of_queries))
 
 
 if __name__ == '__main__':
