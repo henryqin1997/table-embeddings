8e5f5ff3d1eb5b49b218b9b62fd8ec373833d12c
Zian Ke <kezian@umich.edu>
2019-01-17 18:50:06 -0500

    Try linear cosine similarity on 100k

--- numerical_space/falconn.py
+++ numerical_space/falconn.py
@@ -8,14 +8,16 @@ import math
 
 
 def falconn_test():
-    train_size = 100
+    train_size = 103000
     batch_size = 50
-    batch_index = 0
-    number_of_queries = 10
+    number_of_queries = 3000
+
+    print('Load 103k data')
     summary = [load(batch_size=batch_size, batch_index=batch_index) for batch_index in
                range(int(math.floor(train_size / batch_size)))]
     dataset = list(itertools.chain.from_iterable(itertools.chain.from_iterable(summary)))
 
+    print('Filter data')
     # Filter out mean, variance, min, max all zero
     dataset = list(filter(lambda l: np.linalg.norm(l[1:5]) > 0, dataset))
 
@@ -23,14 +25,15 @@ def falconn_test():
 
     vectors = np.array([l[1:5] for l in dataset_asc_int])
     vectors /= np.linalg.norm(vectors, axis=1).reshape(-1, 1)
-    labels = np.array([l[0] for l in dataset_asc_int])
-    print(vectors)
-    print(labels)
+    labels = [l[0] for l in dataset_asc_int]
 
+    print('Predict last 3k')
     answers = []
     for query in vectors[-number_of_queries:]:
         answers.append(labels[np.dot(vectors[:-number_of_queries], query).argmax()])
-    print(answers)
+
+    assert len(answers) == number_of_queries
+    print('Accuracy: {}/{}'.format(sum(i == j for i, j in zip(answers, labels[-number_of_queries:])), number_of_queries))
 
 
 if __name__ == '__main__':
