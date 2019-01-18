eb95597ada2904b662ac06e947c5fbf2c43eab8e
Zian Ke <kezian@umich.edu>
2019-01-17 20:52:29 -0500

    Predict on 6 cases

--- numerical_space/falconn.py
+++ numerical_space/falconn.py
@@ -3,11 +3,14 @@ from numerical_space.load import load
 import itertools
 import math
 
+filter_lambdas = [lambda l: not l[6] and l[5] == 1, lambda l: l[6] and l[5] == 1, lambda l: not l[6] and l[5] == 0,
+                  lambda l: l[6] and l[5] == 0, lambda l: not l[6] and l[5] == -1, lambda l: l[6] and l[5] == -1]
+filter_names = ['asc int', 'asc float', 'rand int', 'rand float', 'desc int', 'desc float']
+
 
 def falconn_test():
     train_size = 103000
     batch_size = 50
-    number_of_queries = 3000
 
     print('Load 103k data')
     summary = []
@@ -17,29 +20,34 @@ def falconn_test():
 
     dataset = list(itertools.chain.from_iterable(itertools.chain.from_iterable(summary)))
 
-    print('Filter data')
     # Filter out mean, variance, min, max all zero
     dataset = list(filter(lambda l: np.linalg.norm(l[1:5]) > 0, dataset))
 
-    # Filter data in ascending order and is integer
-    dataset_asc_int = list(filter(lambda l: not l[6] and l[5] == 1, dataset))
+    for filter_lambda, filter_name in zip(filter_lambdas, filter_names):
+        print('Predict ' + filter_name)
+        dataset_filtered = list(filter(filter_lambda, dataset))
+        vectors = np.array([l[1:5] for l in dataset_filtered])
 
-    vectors = np.array([l[1:5] for l in dataset_asc_int])
-    vectors /= np.linalg.norm(vectors, axis=1).reshape(-1, 1)
-    labels = [l[0] for l in dataset_asc_int]
+        number_of_queries = int(round(len(vectors) * 0.1))
+        print('Vector length: {}, Number of queries: {}'.format(len(vectors), number_of_queries))
 
-    print('Predict last 3k')
-    answers = []
-    for k, query in enumerate(vectors[-number_of_queries:]):
-        answers.append(labels[np.dot(vectors[:-number_of_queries], query).argmax()])
-        if k % 100 == 9:
-            print('{}/{} - Accuracy: {}'.format(
-                k + 1,
-                number_of_queries,
-                vector_accuracy(answers,
-                                labels[len(labels) - number_of_queries:len(labels) - number_of_queries + k + 1])))
+        if number_of_queries == 0:
+            continue
 
-    print('Accuracy: {}'.format(vector_accuracy(answers, labels[-number_of_queries:])))
+        vectors /= np.linalg.norm(vectors, axis=1).reshape(-1, 1)
+        labels = [l[0] for l in dataset_filtered]
+
+        answers = []
+        for k, query in enumerate(vectors[-number_of_queries:]):
+            answers.append(labels[np.dot(vectors[:-number_of_queries], query).argmax()])
+            # if k % 100 == 9:
+            #     print('{}/{} - Accuracy: {}'.format(
+            #         k + 1,
+            #         number_of_queries,
+            #         vector_accuracy(answers,
+            #                         labels[len(labels) - number_of_queries:len(labels) - number_of_queries + k + 1])))
+
+        print('Accuracy {}: {}'.format(filter_name, vector_accuracy(answers, labels[-number_of_queries:])))
 
 
 def vector_accuracy(a, b):
