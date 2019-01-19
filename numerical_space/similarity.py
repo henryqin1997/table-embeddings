5e9158a1f9cf834e1dcf3b9f97c21412f2bf7ea4
Zian Ke <kezian@umich.edu>
2019-01-19 14:13:53 -0500

    Generate report

--- numerical_space/similarity.py
+++ numerical_space/similarity.py
@@ -3,10 +3,13 @@ from numerical_space.load import load
 import itertools
 import math
 import os
+import json
+from collections import defaultdict
 
 filter_lambdas = [lambda l: not l[6] and l[5] == 1, lambda l: l[6] and l[5] == 1, lambda l: not l[6] and l[5] == 0,
                   lambda l: l[6] and l[5] == 0, lambda l: not l[6] and l[5] == -1, lambda l: l[6] and l[5] == -1]
 filter_names = ['asc int', 'asc float', 'rand int', 'rand float', 'desc int', 'desc float']
+wordlist = json.load(open('data/wordlist_v6_index.json'))
 
 
 def falconn_test():
@@ -55,6 +58,8 @@ def falconn_test():
         print('Accuracy {}: {}'.format(filter_name, vector_accuracy(labels[-number_of_queries:], answers)))
         print('No other accuracy {}: {}'.format(filter_name,
                                                 vector_accuracy_no_other(labels[-number_of_queries:], answers)))
+        generate_report(labels[-number_of_queries:], answers,
+                        'numerical_space/dict_{}.json'.format(filter_name.replace(' ', '_')))
 
 
 def vector_accuracy(a, b):
@@ -65,6 +70,24 @@ def vector_accuracy(a, b):
 def vector_accuracy_no_other(a, b, other=3333):
     no_other = list(filter(lambda item: item[0] != other, zip(a, b)))
     return sum(i == j for i, j in no_other) / len(no_other)
+
+
+def get_label_by_index(index):
+    try:
+        return list(wordlist.items())[int(index)][0]
+    except IndexError:
+        return 'OTHER'
+
+
+def generate_report(a, b, filename, other=3333):
+    report = {}
+    for i, j in zip(a, b):
+        if i != other:
+            label = get_label_by_index(i)
+            if label not in report:
+                report[label] = defaultdict(int)
+            report[label][get_label_by_index(j)] += 1
+    json.dump(report, open(filename, 'w+'), indent=4)
 
 
 if __name__ == '__main__':
