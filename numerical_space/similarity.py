8de43d5581e67ea5336c54cf3da45fe6a020ebc3
Zian Ke <kezian@umich.edu>
2019-01-19 12:26:33 -0500

    Save dataset csv

--- numerical_space/similarity.py
+++ numerical_space/similarity.py
@@ -2,6 +2,7 @@ import numpy as np
 from numerical_space.load import load
 import itertools
 import math
+import os
 
 filter_lambdas = [lambda l: not l[6] and l[5] == 1, lambda l: l[6] and l[5] == 1, lambda l: not l[6] and l[5] == 0,
                   lambda l: l[6] and l[5] == 0, lambda l: not l[6] and l[5] == -1, lambda l: l[6] and l[5] == -1]
@@ -13,12 +14,16 @@ def falconn_test():
     batch_size = 50
 
     print('Load 103k data')
-    summary = []
-    for batch_index in range(int(math.floor(train_size / batch_size))):
-        print(batch_index)
-        summary.append(load(batch_size=batch_size, batch_index=batch_index))
+    if os.path.isfile('numerical_space/dataset.csv'):
+        dataset = np.genfromtxt('numerical_space/dataset.csv', delimiter=',')
+    else:
+        summary = []
+        for batch_index in range(int(math.floor(train_size / batch_size))):
+            print(batch_index)
+            summary.append(load(batch_size=batch_size, batch_index=batch_index))
 
-    dataset = list(itertools.chain.from_iterable(itertools.chain.from_iterable(summary)))
+        dataset = np.array(list(itertools.chain.from_iterable(itertools.chain.from_iterable(summary))))
+        np.savetxt('numerical_space/dataset.csv', dataset, delimiter=',')
 
     # Filter out mean, variance, min, max all zero
     dataset = list(filter(lambda l: np.linalg.norm(l[1:5]) > 0, dataset))
