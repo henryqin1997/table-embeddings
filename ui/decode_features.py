e0834e1d4e2407eaecde9383414772d1f5031a33
Zian Ke <kezian@umich.edu>
2019-02-03 22:49:05 -0500

    UI to decode features

---
+++ ui/decode_features.py
@@ -0,0 +1,36 @@
+import sys
+import re
+import json
+
+tag_to_index = {'LOCATION': 0, 'PERSON': 1, 'ORGANIZATION': 2}
+tags = list(tag_to_index.keys())
+
+
+def value_to_features(value):
+    if value < 0:
+        return 'NULL'
+    features = {}
+    bools = list(map(int, list('{0:b}'.format(int(value)).zfill(11))))
+    features['TEXT'] = bools[-1]
+    features['SYMBOL'] = bools[-2]
+    features['NUMBER'] = bools[-3]
+    features['LOCATION'] = bools[-4]
+    features['PERSON'] = bools[-5]
+    features['ORGANIZATION'] = bools[-6]
+    features['DATE'] = bools[-7]
+    features['IS_NUMERIC'] = bools[-8]
+    features['IS_FLOAT'] = bools[-9]
+    if bools[-10] == 0 and bools[-11] == 0:
+        features['IS_ORDERED'] = 'rand'
+    elif bools[-10] == 1 and bools[-11] == 0:
+        features['IS_ORDERED'] = 'desc'
+    elif bools[-10] == 0 and bools[-11] == 1:
+        features['IS_ORDERED'] = 'asc'
+    else:
+        features['IS_ORDERED'] = 'error'
+    return features
+
+
+if __name__ == '__main__':
+    value = sys.argv[1]
+    print(value_to_features(int(value)))
