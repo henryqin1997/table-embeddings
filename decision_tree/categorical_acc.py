05f29a584fa2e00320970a83ab6bef81c5ec1b70
henryqin <henryqin@umich.edu>
2019-02-18 14:28:10 -0500

    categorical accuracy sort function

--- decision_tree/categorical_acc.py
+++ decision_tree/categorical_acc.py
@@ -5,20 +5,20 @@ from .decision_tree import label_num_arr, label_num_str, label_num_str_no_other,
 
 
 def cal_accuracy():
-    #dic_no_cut = json.load(open('decision_tree/diction_nst{}.json'.format("10digits")))
+    dic_no_cut = json.load(open('decision_tree/diction_nst{}.json'.format("10digits")))
     #dic_prediction = json.load(open('decision_tree/diction_nst_prediction{}.json'.format("10digits")))
     #dic_cut_pred = json.load(open('decision_tree/dic_nst_cut_pred{}.json'.format("10digits")))
 
-    dic_no_cut = json.load(open('decision_tree/diction.json'))
+    #dic_no_cut = json.load(open('decision_tree/diction.json'))
     # dic_prediction = json.load(open('decision_tree/diction_prediction_with0.json'))
     # dic_cut_pred = json.load(open('decision_tree/dic_cut_pred.json'))
 
-    #categorical_acc = defaultdict(lambda: [0, 0, 0])
+    categorical_acc = defaultdict(lambda: [0, 0, 0])
 
-    pre_acc = 0
-    sum = 0
-    sum_no_other = 0
-    acc_no_other = 0
+    # pre_acc = 0
+    # sum = 0
+    # sum_no_other = 0
+    # acc_no_other = 0
 
     for key in dic_no_cut.keys():
         max = 0
@@ -28,13 +28,13 @@ def cal_accuracy():
         for label in dic_no_cut[key].keys():
             if label != '-1,-1,-1,-1,-1,-1,-1,-1,-1,-1':
 
-                sum += dic_no_cut[key][label] * label_num_str(label)
-                sum_no_other += dic_no_cut[key][label] * label_num_str_no_other(label)
+                # sum += dic_no_cut[key][label] * label_num_str(label)
+                # sum_no_other += dic_no_cut[key][label] * label_num_str_no_other(label)
 
-                # cats = label.split(',')
-                # for cat in cats:
-                #     if cat != '-1':
-                #         categorical_acc[cat][0] += dic_no_cut[key][label]
+                cats = label.split(',')
+                for cat in cats:
+                    if cat != '-1':
+                        categorical_acc[cat][0] += dic_no_cut[key][label]
 
                 if dic_no_cut[key][label] > max:
                     max = dic_no_cut[key][label]
@@ -46,24 +46,24 @@ def cal_accuracy():
         cat_max = maxlabel.split(',')
 
         for label in dic_no_cut[key].keys():
-            pre_acc += dic_no_cut[key][label] * correct_pred(maxlabel, label)
-            acc_no_other += dic_no_cut[key][label] * correct_pred_no_other(maxlabel, label)
-            # cats = label.split(',')
-            # for i, cat in enumerate(cats):
-            #     if cat != '-1':
-            #         if cat_max[i] == cat:
-            #             categorical_acc[cat][1] += dic_no_cut[key][label]
+            # pre_acc += dic_no_cut[key][label] * correct_pred(maxlabel, label)
+            # acc_no_other += dic_no_cut[key][label] * correct_pred_no_other(maxlabel, label)
+            cats = label.split(',')
+            for i, cat in enumerate(cats):
+                if cat != '-1':
+                    if cat_max[i] == cat:
+                        categorical_acc[cat][1] += dic_no_cut[key][label]
 
-    # for key in categorical_acc.keys():
-    #     categorical_acc[key][2] = categorical_acc[key][1] / categorical_acc[key][0]
+    for key in categorical_acc.keys():
+        categorical_acc[key][2] = categorical_acc[key][1] / categorical_acc[key][0]
 
-    print("train accuracy {}".format(pre_acc / sum))
-    print("train accuracy no other {}".format(acc_no_other / sum_no_other))
+    # print("train accuracy {}".format(pre_acc / sum))
+    # print("train accuracy no other {}".format(acc_no_other / sum_no_other))
 
-    # with open('decision_tree/categorical_acc_previous.json', 'w') as fp:
-    #     categorical_acc = dict(sorted(categorical_acc.items(), key=lambda item: item[1][0], reverse=True))
-    #     # categorical_acc = dict(sorted(categorical_acc.items(), key=lambda item: item[1][2]))
-    #     json.dump(categorical_acc, fp, indent=4)
+    with open('decision_tree/categorical_acc_previous.json', 'w') as fp:
+        categorical_acc = dict(sorted(categorical_acc.items(), key=lambda item: item[1][0], reverse=True))
+        # categorical_acc = dict(sorted(categorical_acc.items(), key=lambda item: item[1][2]))
+        json.dump(categorical_acc, fp, indent=4)
 
 
 def cal_head_acc():
