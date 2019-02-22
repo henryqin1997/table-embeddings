0204d14a830c8c9d2101892850405d721a270ec8
henryqin <henryqin@umich.edu>
2019-02-22 13:29:50 -0500

    nn model

--- nn/softmax.py
+++ nn/softmax.py
@@ -1,4 +1,6 @@
 import torch.nn as nn
+import torch
+import torch.optim as optim
 import time
 import torch.nn.functional as F
 
@@ -61,20 +63,9 @@ def test(testloader, net, device):
 def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     # device = torch.device('cpu')
-    transform = transforms.Compose(
-        [transforms.ToTensor(),
-         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                            download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=100,
-                                              shuffle=True)
 
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                           download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=100,
-                                             shuffle=False)
-    net = VGG().to(device)
+    net = Table().to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(net.parameters(), lr=0.001)
 
