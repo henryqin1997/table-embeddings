4acca634fc56b4608675ebadf413bbe833a67c44
Zian Ke <kezian@umich.edu>
2019-02-23 14:26:09 -0500

    Fix bug of nn

--- nn/softmax.py
+++ nn/softmax.py
@@ -4,25 +4,32 @@ import torch.optim as optim
 import torch.utils.data
 import numpy as np
 import time
-from .load import load_data, load_data_100_sample
+from .load import load_data, load_data_100_sample, load_data_domain_schemas
 
 torch.manual_seed(1)
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+input_size = 120
+hidden_size = 500
+num_labels = 3334
+num_epochs = 10
+batch_size = 50
+learning_rate = 0.1
+test_size = 2000
 
-class Table(nn.Module):
+
+class NeuralNet(nn.Module):
     def __init__(self):
-        super(Table, self).__init__()
+        super(NeuralNet, self).__init__()
         self.fc = nn.Sequential(
-            # fc 100*300
-            # relu
-            # fc 300*3000
-            nn.Linear(120, 300),
+            nn.Linear(input_size, hidden_size),
             nn.ReLU(),
-            nn.Linear(300, 3334)
+            nn.Linear(hidden_size, num_labels)
         )
 
     def forward(self, x):
-        x = self.fc(x)
+        out = self.fc(x)
+        return out
 
 
 class TableDataset(torch.utils.data.Dataset):
@@ -40,66 +47,64 @@ class TableDataset(torch.utils.data.Dataset):
         return self.inputs[index], self.targets[index]
 
 
-def train(train_loader, net, criterion, optimizer, device):
-    for epoch in range(10):  # loop over the dataset multiple times
+def train(train_loader, model, criterion, optimizer, device):
+    for epoch in range(num_epochs):  # loop over the dataset multiple times
         start = time.time()
         running_loss = 0.0
 
+        total_iter = len(train_loader)
         for i, (columns, labels) in enumerate(train_loader):
-            columns = columns.to(device)
+            columns = columns.float().to(device)
             labels = labels.to(device)
 
-            optimizer.zero_grad()
-            out = net.forward(columns)
+            out = model(columns)
             loss = criterion(out, labels)
+            optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            # print statistics
+
             running_loss += loss.item()
-            if i % 100 == 99:  # print every 2000 mini-batches
+            if (i + 1) % 100 == 0:
                 end = time.time()
-                print('[epoch %d, iter %5d] loss: %.3f eplased time %.3f' %
-                      (epoch + 1, i + 1, running_loss / 100, end - start))
+                print('Epoch [{}/{}], Iter [{}/{}], Loss: {:.3f}, Elapsed time {:.3f}'.format(
+                    epoch + 1, num_epochs, i + 1, total_iter, running_loss / 100, end - start))
                 start = time.time()
                 running_loss = 0.0
     print('Finished Training')
 
 
-def test(test_loader, net, device):
+def test(test_loader, model, device):
     correct = 0
     total = 0
     with torch.no_grad():
         for data in test_loader:
             columns, labels = data
-            columns = columns.to(device)
+            columns = columns.float().to(device)
             labels = labels.to(device)
-            outputs = net(columns)
+            outputs = model(columns)
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
-    print('Accuracy of the network on the 10000 test columns: %d %%' % (
-            100 * correct / total))
+    print('Accuracy of the network on the test columns: {}%'.format(
+        100 * correct / total))
 
 
 def main():
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    # device = torch.device('cpu')
-
-    net = Table().to(device)
+    model = NeuralNet().to(device)
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(net.parameters(), lr=0.1)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-    inputs, targets = load_data_100_sample()
+    inputs, targets = load_data_domain_schemas()
     dataset = TableDataset(inputs, targets)
 
-    train_dataset, test_dataset = torch.utils.data.random_split(dataset, [len(dataset) - 6, 6])
+    train_dataset, test_dataset = torch.utils.data.random_split(dataset, [len(dataset) - test_size, test_size])
 
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=10,
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size,
                                                shuffle=True)
-    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=10,
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size,
                                               shuffle=False)
-    train(train_loader, net, criterion, optimizer, device)
-    test(test_loader, net, device)
+    train(train_loader, model, criterion, optimizer, device)
+    test(test_loader, model, device)
 
 
 if __name__ == "__main__":
